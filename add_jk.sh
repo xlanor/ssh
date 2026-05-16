5d27df3dc69f5d03f2e1eb282244eaa091872e18
jingkai. <contact@jingk.ai>
2026-05-16 23:04:38 +0100

    fix2

--- add_jk.sh
+++ add_jk.sh
@@ -8,10 +8,10 @@ mkdir -p /home/jingkai/.ssh
 
 chmod 700 /home/jingkai/.ssh
 
-touch /home/jingkai/authorized_keys
+touch /home/jingkai/.ssh/authorized_keys
 
-echo authorized_keys >> /home/jingkai/authorized_keys
+echo authorized_keys >> /home/jingkai/.ssh/authorized_keys
 
-chmod 600 /home/jingkai/authorized_keys
+chmod 600 /home/jingkai/.ssh/authorized_keys
 
 sudo chown -R jingkai:jingkai /home/jingkai/.ssh
