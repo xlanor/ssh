879a089bf24fb63c00f99e9d7748ed7767c70dec
jingkai. <contact@jingk.ai>
2026-05-16 23:02:58 +0100

    relative

--- add_jk.sh
+++ add_jk.sh
@@ -4,18 +4,14 @@ useradd -m jingkai -s /bin/bash
 
 sudo passwd jingkai
 
-cd /home/jingkai
+mkdir -p /home/jingkai/.ssh
 
-mkdir -p .ssh
+chmod 700 /home/jingkai/.ssh
 
-chmod 700 .ssh
+touch /home/jingkai/authorized_keys
 
-cd .ssh
+echo authorized_keys >> /home/jingkai/authorized_keys
 
-touch authorized_keys
-
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPLKXbiBDCDAdeL3/f9TQOarR7Sa48NOpPJjF4PoZrc4qITtrtyI6JGoH3ICzOV1KABzAXpa9WXaevai5SHy2I9JOpuBVeZsdz6m3NW35TqRkqydbcmbPdPbnNMvNIIxipZfH+wK6DzDGi6zawimhSTOjGc7RGG3UXHk6NQqrqL1oT18KN0WRhEgOBYyWlM65FjRwP/6jh3/GZcIIDj2MM3T9xod5Dwa2jnNiUq4O/xHU5sqp2LLq4+9y4orTZdig+2ktEdykMMOiJxKyDWJPjSkcDqDStDQwj+R5GrE3NpG2wuxRsEtkbn09Eh+KAHP1BrDh0St+L6HcHMN9YwwVl contact@jingk.ai" > authorized_keys
-
-chmod 600 authorized_keys
+chmod 600 /home/jingkai/authorized_keys
 
 sudo chown -R jingkai:jingkai /home/jingkai/.ssh
