84a17d33d09a9f31db4fe34f6f2f12ca60cb4473
jingkai. <contact@jingk.ai>
2026-05-17 12:13:12 +0100

    Update sort command to use output redirection option

--- deploy_root.sh
+++ deploy_root.sh
@@ -1,3 +1,3 @@
 cat ./authorized_keys >> /root/.ssh/authorized_keys
-sort -u /root/.ssh/authorized_keys > /root/.ssh/authorized_keys
+sort -u /root/.ssh/authorized_keys -o /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
